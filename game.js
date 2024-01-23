43b7cba4d87bb412bc65abb208cdb50f75a305bc
Tishaya <118938566+Kiyo-io@users.noreply.github.com>
2024-01-23 02:44:48 -0500

    cleaning up code to continue to step 3

--- game.js
+++ game.js
@@ -1,84 +1,57 @@
 //Step 2
 var gamePattern = [];
-//Successful
+
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var randomChosenColour;
 
-//Successful 
-//Switch statement is inside of fucntion so that it can use the result from the randomNumber variable or the inital result of the fucntion otherwise the switch statement won't know what the randomNumber variable is valued as and so cannot continue the the code ie. act on the switch statement
+//Successful
+//Switch statement is inside of fucntion so that it can use the result from the randomNumber variable ie. the inital result of the fucntion otherwise the switch statement won't know what the randomNumber variable is valued as if the function is and so cannot continue the the code ie. act on the switch statement
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  console.log(randomNumber);
 
+  console.log(randomNumber);
 
   switch (randomNumber) {
     case 0:
       randomChosenColour = buttonColours[0];
       break;
-  
+
     case 1:
       randomChosenColour = buttonColours[1];
       break;
-  
+
     case 2:
       randomChosenColour = buttonColours[2];
       break;
-  
+
     case 3:
       randomChosenColour = buttonColours[3];
       break;
-  
+
     default:
       console.error("Oops! Invalid Random Number!");
   }
   console.log(randomChosenColour);
-}
-
-nextSequence();
-
-
-
-
-switch (randomNumber) {
-  case 0:
-    randomChosenColour = buttonColours[0];
-    break;
-
-  case 1:
-    randomChosenColour = buttonColours[1];
-    break;
-
-  case 2:
-    randomChosenColour = buttonColours[2];
-    break;
-
-  case 3:
-    randomChosenColour = buttonColours[3];
-    break;
-
-  default:
-    console.error("Oops! Invalid Random Number!");
-}
-
-function addToGamePattern(randomChosenColour) {
+  // This code that I incorporated adds the result of the random Chosen Colour to the gamePattern array
   gamePattern.push(randomChosenColour);
 
   console.log(gamePattern);
 }
 
+nextSequence();
+
 //jQuery Step 3
 
-function (randomChosenColour) {
-  $("#red").delay(100).fadeOut().fadeIn("slow");
-};
+// function (randomChosenColour) {
+//   $("#red").delay(100).fadeOut().fadeIn("slow");
+// };
 
-$("#blue").delay(100).fadeOut().fadeIn("slow");
+// $("#blue").delay(100).fadeOut().fadeIn("slow");
 
-$("#yellow").delay(100).fadeOut().fadeIn("slow");
+// $("#yellow").delay(100).fadeOut().fadeIn("slow");
 
-$("#green").delay(100).fadeOut().fadeIn("slow");
-
+// $("#green").delay(100).fadeOut().fadeIn("slow");
 
 // $("#red").click(
 // var redAudio = new Audio("sounds/red.mp3");
