15c0b6cd580aacfa2d6fabd65721257e7bfed036
Tishaya <118938566+Kiyo-io@users.noreply.github.com>
2024-01-23 02:04:27 -0500

    moved switch function into nextSequence function to get value from randomNumber for randomChosenColour

--- game.js
+++ game.js
@@ -2,15 +2,41 @@
 var gamePattern = [];
 //Successful
 var buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChosenColour;
+
 //Successful
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
+
+
+  switch (randomNumber) {
+    case 0:
+      randomChosenColour = buttonColours[0];
+      break;
+  
+    case 1:
+      randomChosenColour = buttonColours[1];
+      break;
+  
+    case 2:
+      randomChosenColour = buttonColours[2];
+      break;
+  
+    case 3:
+      randomChosenColour = buttonColours[3];
+      break;
+  
+    default:
+      console.error("Oops! Invalid Random Number!");
+  }
 }
 
 nextSequence();
 
-var randomChosenColour;
+console.log('Random Chosen Colour:', randomChosenColour);
+
 
 switch (randomNumber) {
   case 0:
